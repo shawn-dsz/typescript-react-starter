a890c07c1b0a4857e247ca46a358d2d5073e72fc
Shawn <shdsouza@seek.com.au>
2021-12-17 16:29:48 +1100

    rm styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <div className="container">Hello</div>
     </div>
   );
 }
